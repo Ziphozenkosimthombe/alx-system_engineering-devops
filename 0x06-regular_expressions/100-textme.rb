2cac9f773ccea94b00d9879c21a3070d5c7df8b9
Ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-10-03 06:26:32 -0700

    regular_expressions

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 #  regular expression that will match the given expression pattern.
-puts ARGV[0].scan(/[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
+puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')

47e76d4e6e43661a54f47a83ca25b03f08c7a15e
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-24 08:38:34 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # installing flask from pip3 using Puppet
-exec { 'install python packages':
-     command   => 'pip3 install flask flask_restful apiai',
-     path      => ['/usr/bin/'],
-     unless    => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+package{ 'flask':
+  ensure    => '2.1.0',
+  provider  => 'pip3',
 }

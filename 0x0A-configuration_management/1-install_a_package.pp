6329c8588519b828ab8ec016cb99e3905b117c78
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-24 10:39:17 -0800

    configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# installing flask from pip3 using Puppet
-package{ 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
+# insatlling flask from pip3 using Puppet
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.0.2',
+  path    => '/usr/bin',
+  require => Package['python3-pip'],
 }

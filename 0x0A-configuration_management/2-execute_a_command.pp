fabb83f326be20548a23020d6873be7771bd7c95
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-11-25 01:31:10 -0800

    configuration_management

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # kills a process named killmenow
 exec { 'killmenow':
-  command      => 'pkill -f killmenow',
-  path         => '/usr/bin:/bin',
-  onlyif       => 'pgrep -f killmenow',
-  logoutput    => true,
+  command   => 'pkill -f killmenow',
+  path      => '/usr/bin:/bin',
+  onlyif    => 'pgrep -f killmenow',
+  logoutput => true,
 }

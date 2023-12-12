f3866527469b6c9b09e31db7454fd8182edd4c38
ziphozenkosimthombe <ziphomthombe478@gmail.com>
2023-12-12 03:55:34 -0800

    task_02

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,16 @@
-# configure an Nginx
+# Use Puppet to automate the task of creating a custom HTTP header response
 
-package{ 'nginx':
-  ensure => 'installed',
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-service{ 'nginx':
-  ensure => 'running',
-  enable => 'true',
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file{'/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server ipv6only=on;
-    add_header X-Served-By ${hostname};
-    root /var/www/html;
-    index index.html index.htm;
-    server_name localhost;
-    error_page 404 /404.html;
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}",
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-  replace => 'true',
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file {'/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-  }
-
-file {'/var/www/html/404.html':
-  ensure  => 'file',
-  content => "Ceci n'est pas une page",
-  require => Package['nginx'],
-  notify  => Service['nginx'],}
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
+}
